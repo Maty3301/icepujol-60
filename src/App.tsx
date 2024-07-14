3be30ccf5efbbdd46641bae14b8a39a63da75cb1
miqueas.alvarez <miqueas.alvarez@mi.unc.edu.ar>
2024-07-14 13:30:21 -0300

    en construccion

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,31 @@ import Programs from "./components/Programs/Programs";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Title from "./components/Title/Title";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
-    const [playState, setPlayState] = useState(false);
+  const [playState, setPlayState] = useState(false);
 
-    return (
-        <div>
-            <SpeedInsights />
-            <Navbar />
-            <Hero />
-            <div className="container">
-                <Title subtitle="Actividades" title="Que Ofrecemos" />
-                <Programs />
-                <About setPlayState={setPlayState} />
-                <Title subtitle="Galería" title="Fotos de Actividades" />
-                <Campus />
-                <Title subtitle="Testimonios" title="Que Decimos Nosotros" />
-                <Testimonials />
-                <Title subtitle="Contáctanos" title="Póngase en Contacto" />
-                <Contact />
-            </div>
-            <Footer />
-            <VideoPlayer playState={playState} setPlayState={setPlayState} />
-        </div>
-    );
+  return (
+    <div>
+      <SpeedInsights />
+      <Navbar />
+      <Hero />
+      {/* <div className="container">
+        <Title subtitle="Actividades" title="Que Ofrecemos" />
+        <Programs />
+        <About setPlayState={setPlayState} />
+        <Title subtitle="Galería" title="Fotos de Actividades" />
+        <Campus />
+        <Title subtitle="Testimonios" title="Que Decimos Nosotros" />
+        <Testimonials />
+        <Title subtitle="Contáctanos" title="Póngase en Contacto" />
+        <Contact />
+      </div>
+      <Footer />
+      <VideoPlayer playState={playState} setPlayState={setPlayState} /> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,22 +2,20 @@ import "./Hero.css";
 import darkArrow from "../../assets/dark-arrow.png";
 
 const Hero = () => {
-    return (
-        <div className="hero container">
-            <div className="hero-text">
-                <h1>Garantizamos una educación mejor para un mundo mejor</h1>
-                <p>
-                    Nuestro plan de estudios de vanguardia está diseñado para
-                    capacitar a los estudiantes con los conocimientos,
-                    habilidades y experiencias necesarias para sobresalir en el
-                    dinámico campo de la educación
-                </p>
-                <button className="btn">
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>BAJO CONSTRUCCIÓN</h1>
+        <p>
+          Nuestro sitio web está en construcción, ¡pero estamos listos para
+          comenzar! Estamos preparando algo increíble y emocionante para ti
+        </p>
+        {/* <button className="btn">
                     Explorar Más <img src={darkArrow} alt="darkArrow" />
-                </button>
-            </div>
-        </div>
-    );
+                </button> */}
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
